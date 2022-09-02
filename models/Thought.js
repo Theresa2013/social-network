a06aa4833f09508354d782bbd7605e2cb5804ad1
Theresa2013 <tab61295@gmail.com>
2022-09-02 08:41:08 -0500

    add dateFormat

---
+++ models/Thought.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+
+    },
+    createdAt: {
+
+    }
+})
